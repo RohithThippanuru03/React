947f5bd464bf36db438ceeea067ca8543b59ebc1
RohithThippanuru03 <rohithreddythippanuru03@gmail.com>
2024-09-02 19:46:28 -0500

    first commit

---
+++ src/Rohith.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sahiti from './Sahiti';
+
+function Rohith() {
+  return (
+    <div className="App">
+      <p>Hi, Thiss is Rohith, This is my component</p>
+       <Sahiti />
+    </div>
+  );
+}
+
+export default Rohith;
---
+++ src/Sahiti.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Sahiti() {
+  return (
+    <div className="App">
+      <p>Blah blah Blah</p>
+       
+    </div>
+  );
+}
+
+export default Sahiti;
